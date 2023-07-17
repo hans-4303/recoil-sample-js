e2408a1bfd93add1ba33aca47b2ac796527732d7
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-07-17 17:24:26 +0900

    recoil: selector 작성 및 활용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import { useRecoilValue } from "recoil";
-import { todoListState } from "./Recoil/Atoms/todoListState.atom";
+/* before
+import { todoListState } from "./Recoil/Atoms/todoListState.atom"; */
+/* after */
+import { filteredTodoListState } from "./Recoil/Selectors/filteredTodoListState.selector";
 
 import TodoItemCreator from "./Components/TodoItemCreator";
 import TodoItem from "./Components/TodoItem";
+import TodoListStats from "./Components/TodoListStats";
+import TodoListFilters from "./Components/TodoListFilters";
 
 function App() {
   /* useRecoilValue
   원자 또는 선택기(읽기 전용 또는 쓰기 가능)의 값을 반환하고
   구성 요소를 해당 상태의 향후 업데이트에 구독합니다. */
-  const todoList = useRecoilValue(todoListState);
+
+  /* before, 필터 없이 모든 todoList를 호출함
+  const todoList = useRecoilValue(todoListState); */
+
+  /* after, 필터된 TodoList를 호출함 */
+  const todoList = useRecoilValue(filteredTodoListState);
 
   /* 반환되는 컴포넌트 */
   return (
     <div className="App">
-      {/* TodoItemCreator 컴포넌트 반환, 작성과 업데이트 가능 */}
+      {/* TodoItemCreator 컴포넌트 호출 */}
+      <TodoListStats />
+      {/* TodoItemCreator 컴포넌트 호출 */}
+      <TodoListFilters />
+      {/* TodoItemCreator 컴포넌트 호출, 작성과 업데이트 가능 */}
       <TodoItemCreator />
 
       {/* todoList 배열 순회하면서 TodoItem 컴포넌트 호출,

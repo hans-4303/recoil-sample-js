f1a1f6ffc3c65d9d00e4899aaf91aa787170143f
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-07-18 15:06:14 +0900

    엄격 모드가 에러 바운더리 아래에 있도록

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+/* ErrorBoundary는 직접 작성하는 클래스형 컴포넌트 */
 import ErrorBoundary from "./ErrorBoundary";
 
 /* 부모 트리의 RecoilRoot, recoil 패키지에서 가져오기 */
@@ -11,12 +12,14 @@ import { RecoilRoot } from "recoil";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
+    {/* fallback 설정 가능 */}
     <ErrorBoundary fallback={<p>Something went wrong</p>}>
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <React.StrictMode>
+      <React.StrictMode>
+        {/* react.Suspense 호출 가능, fallback으로 로딩 중 불러올 ui 지정 가능 */}
+        <React.Suspense fallback={<div>Loading...</div>}>
           <App />
-        </React.StrictMode>
-      </React.Suspense>
+        </React.Suspense>
+      </React.StrictMode>
     </ErrorBoundary>
   </RecoilRoot>
 );
